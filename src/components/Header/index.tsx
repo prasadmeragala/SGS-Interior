5e3b86bca250c08d7c986647b6770b734d265b24
Prasad Meragala <prasadmeragala17@gmail.com>
2026-03-24 22:59:52 +0530

    gap

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,7 @@ const Header = () => {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className=" h-screen sm:[h-80vh] flex items-center justify-center">
       {/* Background Video */}
       <video
         key={currentVideo}
